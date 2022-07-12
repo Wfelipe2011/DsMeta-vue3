81b66d086a3f9482a370e333cca4568f966a8281
wilson <wfelipe2011@gmail.com>
2022-07-12 05:45:20 -0300

    create table, interface products

---
+++ src/components/TableSales.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+  import { defineProps } from 'vue';
+  import IcNotify from '../assets/img/icons/IcNotify.vue';
+
+  import { PropType } from 'vue';
+  import { IProduct } from '~/interface/IProduct';
+
+  const props = defineProps({
+    products: {
+      type: Array as PropType<IProduct[]>,
+      required: true,
+    },
+  });
+</script>
+<template>
+  <table class="my-6 w-full">
+    <tr class="border-b border-gray-400 text-gray-300">
+      <th class="invisible lg:visible">Id</th>
+      <th class="invisible md:visible">Data</th>
+      <th>Vendedor</th>
+      <th class="invisible lg:visible">Visitas</th>
+      <th class="invisible lg:visible">Vendas</th>
+      <th>Total</th>
+      <th>Notificar</th>
+    </tr>
+    <tr
+      v-for="product in props.products"
+      :key="product.id"
+      class="border-b border-gray-600 text-gray-300"
+    >
+      <td class="invisible lg:visible">{{ product.id }}</td>
+      <td class="invisible md:visible">{{ product.date }}</td>
+      <td>{{ product.seller }}</td>
+      <td class="invisible lg:visible">{{ product.visits }}</td>
+      <td class="invisible lg:visible">{{ product.sales }}</td>
+      <td>{{ product.total }}</td>
+      <td class="flex justify-center">
+        <button class="rounded-md border border-[#FF6B72] p-2 brightness-75 hover:brightness-100">
+          <IcNotify />
+        </button>
+      </td>
+    </tr>
+    {{}}
+  </table>
+</template>
+<style scoped>
+  th {
+    @apply py-2;
+  }
+  td {
+    @apply py-4 text-center text-gray-400;
+  }
+</style>
---
+++ src/interface/IProduct.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  id: number;
+  seller: string;
+  date: string;
+  total: string;
+  visits: number;
+  sales: number;
+}
