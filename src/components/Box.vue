da346a77c10c3acf47aef073ab8c87b0323a2c77
wilson <wfelipe2011@gmail.com>
2022-07-12 05:44:59 -0300

    create component box

---
+++ src/components/Box.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bg-box m-4 rounded-xl p-4 md:m-6 md:p-6 lg:m-10 lg:p-10">
+    <slot></slot>
+  </div>
+</template>
+<style scoped>
+  .bg-box {
+    background: #283142;
+  }
+</style>
