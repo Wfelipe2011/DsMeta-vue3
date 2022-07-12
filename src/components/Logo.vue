463d3d615bbe37b638132fbd0416f09862b718fc
wilson <wfelipe2011@gmail.com>
2022-07-12 05:43:17 -0300

    create components logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bg-logo flex flex-col items-center justify-center py-8 text-white">
+    <img
+      src="../assets/img/logo.png"
+      alt=""
+      class="my-4"
+    />
+    <h1 class="text-lg font-semibold">DSMeta</h1>
+    <h2 class="text-gray-300">
+      Desenvolvido por
+      <a
+        href="https://www.linkedin.com/in/wilson-felipe-725538176/"
+        target="_blank"
+        >Wilson Felipe</a
+      >
+    </h2>
+  </div>
+</template>
+<script setup></script>
+<style scoped>
+  .bg-logo {
+    background: linear-gradient(180deg, #102347 0%, #181818 100%);
+  }
+</style>
